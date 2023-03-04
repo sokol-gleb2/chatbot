09daf988cff329074e80ed5fb800cbb018fc5752
Gleb Sokolovski <h.sokolovskyi@sms.ed.ac.uk>
2023-03-04 12:35:46 +0000

    Second Commit Adding environment variables through dotenv

--- app.js
+++ app.js
@@ -1,34 +1,24 @@
-const OPENAI_API_KEY = "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";
+require('dotenv').config();
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
 
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
   apiKey: OPENAI_API_KEY,
 });
-
 const openai = new OpenAIApi(configuration);
 
-openai.createChatCompletion({
-  engine: "text-davinci-002",
-}).then((response) => {
-  const chatId = response.data.id;
-  return openai.appendMessages({
-    chatId,
-    messages: [{role: "user", content: "Hello world"}],
-  });
-}).then((response) => {
-  const chatId = response.data.id;
-  return openai.getMessages({
-    chatId,
-  });
-}).then((response) => {
-  const messages = response.data.data;
-  console.log(messages[messages.length - 1].text);
+const completion = openai.createChatCompletion({
+  model: "gpt-3.5-turbo",
+  messages: [{role: "user", content: "Hello world"}],
+})
+.then((response) => {
+  const messages = response.data.choices;
+  console.log(messages);
 }).catch((error) => {
   console.error(error);
 });
 
-
-// process.env.OPENAI_API_KEY
 
 
